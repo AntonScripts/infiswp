b09cb7deedce6c4dee1561a3e8c63e3f998d9176
Anton Schmidt <SCH230115@spengergasse.at>
2025-03-03 13:23:05 +0100

    changes

--- 2025-02-18-HÜ/3.sql
+++ 2025-02-18-HÜ/3.sql
@@ -1,1 +1,3 @@
 SELECT name, population, area FROM World WHERE area >= 3000000 OR population >= 25000000;
+
+25e6 / hier kann man 25 Millionen einfach schreiben, das ist kürzer und einfacher zu lesen als 25000000
